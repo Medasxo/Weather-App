62229a4976eb88d7eb61b2f471cace7501b3df5d
Medas <medas02345@gmail.com>
2022-07-25 11:10:08 +0300

    Modify getLocationInformation function, now that the user can change location, aswell as add displayInformation function, that currently takes the object with fetched information

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ class weatherInfo{
         this.temperatures = temperatures;
     }
 }
-async function getLocationInformation() {
+async function getLocationInformation(location) {
     let locationName;
     let listOfTemperatures = [];
-    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Vilnius&APPID=6d59b255cd4521630de93aa232d082f9&units=metric', {mode: 'cors'}, {method: 'GET'})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&APPID=6d59b255cd4521630de93aa232d082f9&units=metric', {mode: 'cors'}, {method: 'GET'})
     if(!response.ok){
         throw new Error(`Error! status: ${response.status}`);
     }
     const data = await response.json();
     locationName = data.city.name;
     listOfTemperatures = data.list;
-    return new weatherInfo(locationName, listOfTemperatures);
+    let weatherObject = new weatherInfo(locationName, listOfTemperatures);
+    return weatherObject;
 }
 
-getLocationInformation();
+async function displayInformation (){
+    let weatherObject = await getLocationInformation("Vilnius");
+    console.log(weatherObject);
+}
+displayInformation();
    
