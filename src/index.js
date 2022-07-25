6f9bf69ea5074c75694e8fd9403c0da5a6c64ac6
Medas <medas02345@gmail.com>
2022-07-25 11:02:50 +0300

    Add a fetching information function, which will get the data about the city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+class weatherInfo{
+    constructor(name, temperatures){
+        this.name = name;
+        this.temperatures = temperatures;
+    }
+}
+async function getLocationInformation() {
+    let locationName;
+    let listOfTemperatures = [];
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Vilnius&APPID=6d59b255cd4521630de93aa232d082f9&units=metric', {mode: 'cors'}, {method: 'GET'})
+    if(!response.ok){
+        throw new Error(`Error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    locationName = data.city.name;
+    listOfTemperatures = data.list;
+    return new weatherInfo(locationName, listOfTemperatures);
+}
+
+getLocationInformation();
+   
