126d9c333e54051e80653f3aea961f9ca2a9da91
Medas <medas02345@gmail.com>
2022-07-26 15:19:57 +0300

    Modify function to display errors when wrong location is entered by user

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-import './style.css';
-class weatherInfo{
-    constructor(name, temperatures){
-        this.name = name;
-        this.temperatures = temperatures;
-    }
+import "./style.css";
+class weatherInfo {
+  constructor(name, temperatures) {
+    this.name = name;
+    this.temperatures = temperatures;
+  }
 }
 async function getLocationInformation(location) {
-    let locationName;
-    let listOfTemperatures = [];
-    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&APPID=6d59b255cd4521630de93aa232d082f9&units=metric', {mode: 'cors'}, {method: 'GET'})
-    if(!response.ok){
-        throw new Error(`Error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    locationName = data.city.name;
-    listOfTemperatures = data.list;
-    let weatherObject = new weatherInfo(locationName, listOfTemperatures);
-    return weatherObject;
+  let locationName;
+  let listOfTemperatures = [];
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      location +
+      "&APPID=6d59b255cd4521630de93aa232d082f9&units=metric",
+    { mode: "cors" },
+    { method: "GET" }
+  );
+  if (!response.ok) {
+    const errorTab = document.querySelector(".errorTab");
+    errorTab.textContent =
+      "There isn't any location that would match your input";
+    return 0;
+  }
+  const errorTab = document.querySelector(".errorTab");
+  errorTab.textContent = '';
+  const data = await response.json();
+  locationName = data.city.name;
+  listOfTemperatures = data.list;
+  let weatherObject = new weatherInfo(locationName, listOfTemperatures);
+  return weatherObject;
 }
 
-async function displayInformation (location){
-    let weatherObject = await getLocationInformation(location);
+async function displayInformation(location) {
+  let weatherObject = await getLocationInformation(location);
+  if (weatherObject !== 0) {
     let locationName = document.querySelector(".locationName");
     locationName.textContent = weatherObject.name;
     console.log(weatherObject);
+  }
 }
-displayInformation("Vilnius");
-   
+
+const searchBar = document.querySelector(".searchBar");
+searchBar.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    console.log(searchBar.value);
+    displayInformation(searchBar.value);
+  }
+});
+displayInformation("vilnius");
